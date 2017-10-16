df7106092e80191a5aa3651ba3141eed8e94dc6f
AlexRob <alex.rob@gmx.at>
2017-10-17 00:30:37 +0200

    Now it should work perfectly

--- battleship.js
+++ battleship.js
@@ -27,93 +27,121 @@ $(() => {
     clear();
 
     var ships = [ // Actually don't need both x1 and y1, but for logical reasons I'm going to leave them in
-                {length : 5, x1 : 0, x2 : 0, y1 : 0, y2 : 0}      // carrier
-              , {length : 4, x1 : 0, x2 : 0, y1 : 0, y2 : 0}      // battleship
-              , {length : 4, x1 : 0, x2 : 0, y1 : 0, y2 : 0}      // cruiser
-              , {length : 3, x1 : 0, x2 : 0, y1 : 0, y2 : 0}      // submarine
-              , {length : 2, x1 : 0, x2 : 0, y1 : 0, y2 : 0}      // destroyer              
-              ];
+      { length: 5, x1: 0, x2: 0, y1: 0, y2: 0 }        // carrier
+      , { length: 4, x1: 0, x2: 0, y1: 0, y2: 0 }      // battleship
+      , { length: 4, x1: 0, x2: 0, y1: 0, y2: 0 }      // cruiser
+      , { length: 3, x1: 0, x2: 0, y1: 0, y2: 0 }      // submarine
+      , { length: 2, x1: 0, x2: 0, y1: 0, y2: 0 }      // destroyer              
+    ];
 
-    for(let i in ships) {
+    for (let i in ships) {
       placeShip(ships[i]);
     }
 
     function placeShip(ship) {
-      var    dir = Math.round(Math.random())
-        , place = Math.floor(Math.random() * (10 - ship.length));    
-      
-      ship.x1 = place;
-      ship.y1 = place;
+      let dir = Math.round(Math.random())
+        , placeX = 0
+        , placeY = 0;
 
       // horizontal
-      if(dir == 1) {
+      if (dir == 1) {
+        placeX = Math.floor(Math.random() * (10 - ship.length));
+        placeY = Math.floor(Math.random() * 10);
+
+        ship.x1 = placeX;
+        ship.y1 = placeY;
+
         ship.x2 = ship.x1 + ship.length;
         ship.y2 = ship.y1;
       }
       // vertical
       else {
+        placeX = Math.floor(Math.random() * 10);
+        placeY = Math.floor(Math.random() * (10 - ship.length));
+
+        ship.x1 = placeX;
+        ship.y1 = placeY;
+
         ship.x2 = ship.x1;
         ship.y2 = ship.y1 + ship.length;
       }
 
-      while(!validPlace(ship, dir)) {
+      if (!validPlace(ship, dir)) {
         placeShip(ship);
       }
     }
 
     function validPlace(ship, dir) {
-      // Collision is somehow not working and I'm frustrated
-      if(dir == 1) { 
-        if(0 < ship.x1 && $('td[data-r="' + (ship.x1 - 1) + '"][data-c="' + ship.y1 + '"]').hasClass('ship')) {
+      if (dir == 1) {
+        // Check left
+        if (0 < ship.x1 && $('td[data-r="' + ship.y1 + '"][data-c="' + (ship.x1 - 1) + '"]').hasClass('ship')) {
           return false;
         }
-        for(let i = ship.x1; i < ship.x2; i++) {
-          if($('td[data-r="' + i + '"][data-c="' + ship.y1 + '"]').hasClass('ship')) {
+
+        // Check top and bottom
+        for (let i = ship.x1; i < ship.x2; i++) {
+          if ($('td[data-r="' + ship.y1 + '"][data-c="' + i + '"]').hasClass('ship')) {
             return false;
           }
-          if(ship.x1 < i) {
-            if($('td[data-r="' + i + '"][data-c="' + (ship.y1 + 1) + '"]').hasClass('ship')) {
-              return false;     
+          if (ship.x1 < i) {
+            if ($('td[data-r="' + (ship.y1 + 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
+              return false;
             }
           }
-          if(i < ship.x2) {
-            if($('td[data-r="' + i + '"][data-c="' + (ship.y1 - 1) + '"]').hasClass('ship')) {
+          if (i < ship.x2) {
+            if ($('td[data-r="' + (ship.y1 - 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
               return false;
             }
           }
         }
-        for(let i = ship.x1; i < ship.x2; i++) {
-          $('td[data-r="' + i + '"][data-c="' + ship.y1 + '"]').removeClass('water').addClass('ship');
+
+        // Check right
+        if (ship.x2 < 10 && $('td[data-r="' + ship.y1 + '"][data-c="' + (ship.x2 + 1) + '"]').hasClass('ship')) {
+          return false;
+        }
+
+        // Place ship tiles
+        for (let i = ship.x1; i < ship.x2; i++) {
+          $('td[data-r="' + ship.y1 + '"][data-c="' + i + '"]').removeClass('water').addClass('ship');
         }
       } else {
-        if(0 < ship.y1 && $('td[data-r="' + ship.x1 + '"][data-c="' + (ship.y1 - 1) + '"]').hasClass('ship')) {
+        // Check top
+        if (0 < ship.y1 && $('td[data-r="' + (ship.y1 - 1) + '"][data-c="' + ship.x1 + '"]').hasClass('ship')) {
           return false;
         }
-        for(let i = ship.y1; i < ship.y2; i++) {
-          if($('td[data-r="' + ship.x1 + '"][data-c="' + i + '"]').hasClass('ship')) {
+
+        // Check left and right
+        for (let i = ship.y1; i < ship.y2; i++) {
+          if ($('td[data-r="' + i + '"][data-c="' + ship.x1 + '"]').hasClass('ship')) {
             return false;
           }
-          if(ship.y1 < i) {
-            if($('td[data-r="' + (ship.x1 + 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
-              return false;     
+          if (ship.y1 < i) {
+            if ($('td[data-r="' + i + '"][data-c="' + (ship.x1 + 1) + '"]').hasClass('ship')) {
+              return false;
             }
           }
-          if(i < ship.y2) {
-            if($('td[data-r="' + (ship.x1 - 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
+          if (i < ship.y2) {
+            if ($('td[data-r="' + i + '"][data-c="' + (ship.x1 - 1) + '"]').hasClass('ship')) {
               return false;
             }
           }
         }
-        for(let i = ship.y1; i < ship.y2; i++) {
-          $('td[data-r="' + ship.x1 + '"][data-c="' + i + '"]').removeClass('water').addClass('ship');
+
+        // Check bottom
+        if (ship.y2 < 10 && $('td[data-r="' + (ship.y2 + 1) + '"][data-c="' + ship.x1 + '"]').hasClass('ship')) {
+          return false;
+        }
+        // Place ship tiles
+        for (let i = ship.y1; i < ship.y2; i++) {
+          $('td[data-r="' + i + '"][data-c="' + ship.x1 + '"]').removeClass('water').addClass('ship');
         }
       }
       return true;
-  }
+    }
 
     function clear() {
-      for(let i = 0; i < 10; i++) {
-        for(let j = 0; j < 10; j++) {
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
           $('td[data-r="' + i + '"][data-c="' + j + '"]').removeClass('ship').addClass('water');
         }
       }
