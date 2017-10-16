999a35fcd453138e70aac2d4ff1268003773afc1
AlexRob <alex.rob@gmx.at>
2017-10-17 00:53:19 +0200

    Now working completely

--- battleship.js
+++ battleship.js
@@ -78,26 +78,19 @@ $(() => {
           return false;
         }
 
-        // Check top and bottom
-        for (let i = ship.x1; i < ship.x2; i++) {
-          if ($('td[data-r="' + ship.y1 + '"][data-c="' + i + '"]').hasClass('ship')) {
-            return false;
-          }
-          if (ship.x1 < i) {
-            if ($('td[data-r="' + (ship.y1 + 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
-              return false;
-            }
-          }
-          if (i < ship.x2) {
-            if ($('td[data-r="' + (ship.y1 - 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
-              return false;
-            }
-          }
+        // Check right
+        if (ship.x2 < 9 && $('td[data-r="' + ship.y1 + '"][data-c="' + (ship.x2 + 1) + '"]').hasClass('ship')) {
+          return false;
         }
 
-        // Check right
-        if (ship.x2 < 10 && $('td[data-r="' + ship.y1 + '"][data-c="' + (ship.x2 + 1) + '"]').hasClass('ship')) {
-          return false;
+        // Check top and bottom
+        for (let i = ship.x1; i < ship.x2; i++) {
+          if ($('td[data-r="' + (ship.y1 + 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
+            return false;
+          }
+          if ($('td[data-r="' + (ship.y1 - 1) + '"][data-c="' + i + '"]').hasClass('ship')) {
+            return false;
+          }
         }
 
         // Place ship tiles
@@ -110,27 +103,21 @@ $(() => {
           return false;
         }
 
+        // Check bottom
+        if (ship.y2 < 9 && $('td[data-r="' + (ship.y2 + 1) + '"][data-c="' + ship.x1 + '"]').hasClass('ship')) {
+          return false;
+        }
+
         // Check left and right
         for (let i = ship.y1; i < ship.y2; i++) {
-          if ($('td[data-r="' + i + '"][data-c="' + ship.x1 + '"]').hasClass('ship')) {
+          if ($('td[data-r="' + i + '"][data-c="' + (ship.x1 + 1) + '"]').hasClass('ship')) {
             return false;
           }
-          if (ship.y1 < i) {
-            if ($('td[data-r="' + i + '"][data-c="' + (ship.x1 + 1) + '"]').hasClass('ship')) {
-              return false;
-            }
-          }
-          if (i < ship.y2) {
-            if ($('td[data-r="' + i + '"][data-c="' + (ship.x1 - 1) + '"]').hasClass('ship')) {
-              return false;
-            }
+          if ($('td[data-r="' + i + '"][data-c="' + (ship.x1 - 1) + '"]').hasClass('ship')) {
+            return false;
           }
         }
 
-        // Check bottom
-        if (ship.y2 < 10 && $('td[data-r="' + (ship.y2 + 1) + '"][data-c="' + ship.x1 + '"]').hasClass('ship')) {
-          return false;
-        }
         // Place ship tiles
         for (let i = ship.y1; i < ship.y2; i++) {
           $('td[data-r="' + i + '"][data-c="' + ship.x1 + '"]').removeClass('water').addClass('ship');
